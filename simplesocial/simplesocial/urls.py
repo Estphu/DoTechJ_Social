4be64efd2029603cbf434e293ad973b27367d004
Estphu <jaydee20001217@gmail.com>
2023-09-25 18:29:03 +0500

    updated settings.py

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,5 @@ urlpatterns = [
     path('groups/',include('groups.urls')),
     path('blogs/',include('blog.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
