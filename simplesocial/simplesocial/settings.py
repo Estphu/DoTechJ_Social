f9c88fb4ce1f79f295840896df91ae2cad2de475
Estphu <jaydee20001217@gmail.com>
2023-10-01 17:59:42 +0500

    blog styling

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'thanks'
